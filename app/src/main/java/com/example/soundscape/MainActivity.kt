44a624fda67788a38cc3f5e2cd95d77607a79bfd
KIHM-02 <monteroisaac33@gmail.com>
2024-12-04 21:00:02 -0600

    Creation of the project

---
+++ app/src/main/java/com/example/soundscape/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.soundscape
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessageCard(Message("Kevin", "Jetpack Compose"))
+        }
+    }
+}
+
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+}
+
+@Preview(showBackground =  true)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(Message("Kevin", "Jetpack Compose"))
+}
