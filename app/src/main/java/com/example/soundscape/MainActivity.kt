b8e980b2a8810fd643253fa0b5bb61abf0b358e3
KIHM-02 <monteroisaac33@gmail.com>
2024-12-05 00:02:14 -0600

    First try of compose with material3

--- app/src/main/java/com/example/soundscape/MainActivity.kt
+++ app/src/main/java/com/example/soundscape/MainActivity.kt
@@ -1,37 +1,98 @@
 package com.example.soundscape
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soundscape.ui.theme.SoundScapeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MessageCard(Message("Kevin", "Jetpack Compose"))
+            SoundScapeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Kevin", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
-
 
 data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .statusBarsPadding()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profilepicture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
     }
 }
 
-@Preview(showBackground =  true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Kevin", "Jetpack Compose"))
+    SoundScapeTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(Message("Kevin", "Jetpack Compose"))
+        }
+    }
 }
