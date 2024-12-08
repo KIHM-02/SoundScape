c2af77c4f6f0d61bd42afa0588b0c59d09650f17
Petterson04 <ferpetterson@gmail.com>
2024-12-08 12:44:47 -0600

    Create the vist of UserProfile and create class music and controller DATAMUSIC

---
+++ app/src/main/java/com/example/soundscape/model/Music.kt
@@ -0,0 +1,11 @@
+package com.example.soundscape.model
+
+class Music(
+   var nameSong: String,
+    var nameArtist: String,
+    var image: String,
+    var duraton: String,
+   var genre: String,
+    var uri: String,
+) {
+}
--- app/src/main/java/com/example/soundscape/view/PerfilUsuario.kt
+++ app/src/main/java/com/example/soundscape/view/PerfilUsuario.kt
@@ -1,12 +1,12 @@
 package com.example.soundscape.view
-import android.graphics.pdf.models.ListItem
-import com.example.soundscape.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,53 +15,45 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CheckboxDefaults.colors
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.absolutePadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.ui.graphics.Color
-
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.soundscape.R
 import com.example.soundscape.ui.theme.SoundScapeTheme
-import java.time.format.TextStyle
 
 class PerfilUsuario : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            /*SoundScapeTheme {
+            SoundScapeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ){
                    perfilUsuarioView(name = "rels b")
                 }
             }
-        }*/
+        }
     }
 }
 
 @Composable
 fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
+    val items= List(10) {  "Elemento $" }
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Brush
@@ -73,17 +65,21 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
             )
         )
     ){Column{
-        Image(
-            painter = painterResource(R.drawable.profilepicture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-        )
+
+            Image(
+                painter = painterResource(R.drawable.profilepicture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+    
+            )
+
+        
         Row(
             horizontalArrangement = Arrangement.SpaceAround,
             modifier = Modifier
-                .padding(10.dp)
                 .height(100.dp)
                 .fillMaxWidth()
 
@@ -93,17 +89,19 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
                 contentDescription = "Contact profile picture",
                 modifier = Modifier
                     .size(100.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, MaterialTheme.colorScheme.tertiary, CircleShape)
                     .fillMaxWidth()
             )
-            OutlinedButton(onClick = {},
+            OutlinedButton (onClick = {},
                 modifier = Modifier
                     .background(Color(0xFF32B757))
-
+                    .border(1.dp, Color.Black)
             )
             {
                 Text(text = "Editar perfil",
-                    modifier = Modifier
-                        .absolutePadding()
+                    color = Color.White
+
                 )
             }
         }
@@ -114,8 +112,9 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
                 .padding(10.dp)
         )
         Text(
-            text = " Que quieres hacer hoy",
-            style = MaterialTheme.typography.bodyMedium,
+            text = " ¿Que quieres hacer hoy?",
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.White,
             modifier = modifier
                 .padding(10.dp)
                 .align(Alignment.CenterHorizontally)
@@ -126,14 +125,14 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
                 .padding(10.dp)
                 .align(Alignment.CenterHorizontally)
                 .background(Color(0xFF32B757))
-
+                .border(1.dp, Color.Black)
 
         ){
             Text(
                 text = "Crear playlist",
+                color = Color.White,
                 modifier = Modifier
                     .padding(10.dp)
-
             )
 
         }
@@ -146,12 +145,12 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
 
         Text(
             text = "Lista de canciones",
-            style = MaterialTheme.typography.bodyMedium,
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.White,
             modifier = modifier
                 .padding(10.dp)
                 .align(Alignment.CenterHorizontally)
         )
-
 
         LazyColumn (
             modifier = Modifier
@@ -169,7 +168,8 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
         )
         Text(
             text = "Lista de playlists",
-            style = MaterialTheme.typography.bodyMedium,
+            style = MaterialTheme.typography.bodyLarge,
+            color= Color.White,
             modifier = modifier
                 .padding(10.dp)
                 .align(Alignment.CenterHorizontally)
@@ -182,6 +182,16 @@ fun perfilUsuarioView(name: String, modifier: Modifier = Modifier) {
 
     }
 }
+@Composable
+fun ListItem(text: String) {
+        Text(
+            text = text,
+            modifier = Modifier .fillMaxWidth()
+                .padding(8.dp)
+                .background(Color.LightGray)
+                .padding(16.dp)
+        ) }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -191,4 +201,4 @@ fun GreetingPreview() {
 }
 
 
-}
+
